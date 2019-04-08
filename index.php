b6cc79bc071170ff53489193c569c7d08f2cc20e
MartinPrejean <martin.prejean@hetic.net>
2019-04-08 09:49:19 +0200

    Create index file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php $title = '???'; ?>
+<?php $currentPage = 'index'; ?>
+
+<!DOCTYPE html>
+<!--[if lte IE 7]> <html class="ie67 ie678" lang="fr"> <![endif]-->
+<!--[if IE 8]> <html class="ie8 ie678" lang="fr"> <![endif]-->
+<!--[if gt IE 8]><!--><!--<![endif]-->
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo($title); ?></title>
+    <meta name="description" content="Semaine Intensive Back">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="styles/style.css">
+    <link rel="shortcut icon" href="content/favicon.png">
+  </head>
+  <body>
+
+  </body>
+</html>
