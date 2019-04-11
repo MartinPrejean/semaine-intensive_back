8a1fd674b4bc321ce0d6aee4989014fd86ed26eb
AxelThomas <32869796+AxelThomas@users.noreply.github.com>
2019-04-12 00:52:34 +0200

    fix map

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,3 @@
-// const navSlide = () => {
-//     const burger = document.querySelector('.burger');
-//     const nav = document.querySelector('.list');
-//     const navLinks = document.querySelectorAll('.list li');
-
-//     burger.addEventListener('click', () => {
-//         nav.classList.toggle('nav-active');
-
-//         navLinks.forEach((link, index) => {
-//             if(link.style.animation){
-//                 link.style.animation = '';
-//             }
-//             else{
-//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + .5}s`;
-//             }
-//         })
-//     });  
-// }
-
-// navSlide();
-
 const discoverEnter = () => {
     const discover = document.querySelector('.button_discover');
     const container_intro = document.querySelector('.container_description');
